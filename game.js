490a5025f8c97de5b387873164694bb5c5cca7ae
schromya <schromya@gmail.com>
2025-02-02 20:54:47 -0600

    Frog drawn, but not sure why right eyeball white

--- game.js
+++ game.js
@@ -3,49 +3,90 @@ export {};
 
 
 
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext('2d');
+class Frog {
+    constructor(canvas, ctx, x, y) {
+        this.ctx = ctx;
+        this.canvas = canvas
+        this.fill = "#4B5320";
+        this.stroke = "black";
+
+        // (x,y) is the upper left of the frog body as a square
+        this.x = x;
+        this.y = y;
+
+    }
 
 
+    // Frig "square" is 100x80
+    drawFrog() {
+        const x = this.x;
+        const y = this.y;
+
+        ctx.save();
+        
+        ctx.fillStyle = this.fill;
+
+        //Body
+        ctx.beginPath();
+        ctx.moveTo(x+10, y+30);
+        ctx.lineTo(x+80, y+30);
+        ctx.bezierCurveTo(x+80, y+30, x+90, y+50, x+80, y+70);
+        ctx.bezierCurveTo(x+80, y+70, x+50, y+80, x+10, y+70);
+        ctx.bezierCurveTo(x+10, y+70, x, y+50, x+10, y+30);
+        ctx.fill();
+
+        //Eyes
+        this.drawFrogEye(x+10, y);
+        this.drawFrogEye(x+50, y);
+
+        
+        //Mouth
+        ctx.save();
+        ctx.fillStyle = "white";
+        ctx.roundRect(x+20, y+45, 50, 2, 2);
+        ctx.fill();
+        ctx.restore();
+
+        ctx.restore();
+    }
+
+    // (x,y) is lower left of eye socket.
+    drawFrogEye(x, y) {
+
+        // Eye Socket
+        ctx.save();
+        ctx.beginPath();
+        ctx.moveTo(x, y+30);
+        ctx.bezierCurveTo(x, y+30, x+15, y, x+30, y+30);
+        ctx.fill();
+
+        // Eyeball
+        
+        ctx.strokeStyle = "white";
+        ctx.fillStyle = this.fill;
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.arc(x+15, y+30, 7.5, 0, 2*Math.PI);
+        ctx.stroke();
+        ctx.restore();
+
+    }
 
 
-// Frog body
-function drawFrogBody(fill, x, y) {
-    ctx.fillStyle = fill;
-    ctx.strokeStyle = "black"
-    ctx.lineWidth = 10;
+    animate() {
+        ctx.scale(2,2)
+        this.drawFrog()
 
-    ctx.beginPath();
-    ctx.arc(100+x, 100+y, 40, Math.PI-0.5, 0);
-    ctx.lineTo(300+x, 100+y)
-    ctx.arc(400+x, 100+y, 40, Math.PI, 0.5);
-    ctx.lineTo(450+x, 200+y)
-    ctx.lineTo(430+x, 300+y)
-    ctx.lineTo(70+x, 300+y)
-    ctx.lineTo(50+x, 200+y)
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke()
+    }
 }
 
-drawFrogBody("#4B5320", 0, 0)
+
+////////////////////////// MAIN //////////////////////////
+//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////
 
 
-// Eyes
-ctx.strokeStyle = "#FFF";
-ctx.lineWidth = 4
-
-ctx.beginPath();
-ctx.arc(100, 100, 20, 2* Math.PI, 0);
-ctx.stroke();
-
-ctx.beginPath();
-ctx.arc(400, 100, 20, 2* Math.PI, 0);
-ctx.stroke();
-
-
-// Mouth
-ctx.beginPath();
-ctx.arc(250, 300, 150, Math.PI, 0);
-ctx.stroke();
-
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext('2d');
+const frog =  new Frog(canvas, ctx, 100, 100);
+frog.animate();
